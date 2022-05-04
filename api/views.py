a3ae3af9de18cc2d2390cecc2c5b067e7dbbf663
aniket <dummyani26@gmail.com>
2022-05-04 10:54:56 +0530

    Remove unused import from api.views

--- api/views.py
+++ api/views.py
@@ -4,9 +4,7 @@ from classroom.models import Classroom
 from classroom.serializers import ClassroomSerializer
 from drf_spectacular.utils import extend_schema, inline_serializer
 from rest_framework import generics, serializers
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from .serializers import ToReviewSerializer
 
